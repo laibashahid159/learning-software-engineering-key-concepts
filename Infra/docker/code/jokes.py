8b8aca551b74c3ab2ff105f8bdc686c55f9a8456
unknown <laibashahid159@gmail.com>
2023-04-16 02:16:49 +0500

    learning docker

---
+++ Infra/docker/code/jokes.py
@@ -0,0 +1,12 @@
+import random
+
+jokes = [
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "Why did the chicken cross the playground? To get to the other slide!",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why did the coffee file a police report? It got mugged!",
+    "Why did the pirate go on vacation? To get away from his troubles!",
+]
+
+random_joke = random.choice(jokes)
+print(random_joke)
